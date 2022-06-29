75fdb2ceff17c675bc1545d4e8c5a4e88b1257ca
Anivesh Tiwari <anivesh.tiwari@geminisolutions.com>
2022-06-29 23:29:51 +0530

    assignment3

--- src/main/java/com/java/assignment/java8/Repository/Interfaces/AuditableRepository.java
+++
@@ -1,7 +0,0 @@
-package com.java.assignment.java8.Repository.Interfaces;
-
-import com.java.assignment.java8.Model.AuditTable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditableRepository extends JpaRepository<AuditTable, Integer> {
-}
